0e60bd6d363a55095be720ed4a7e2f9515975a09
Steven Williams <stevenwritescode@gmail.com>
2022-04-11 20:17:17 -0600

    fix logo height

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-left">
+      <div class="d-flex flex-sm-shrink-0 align-left">
         <v-img
           :src="require('@/assets/Livication_Logo_NoTagline_Light.svg')"
           class="my-3"
           contain
           fill="white"
-          height="30"
+          max-height="30"
+          max-width="150"
           position="left"
         />
       </div>
 
       <v-spacer></v-spacer>
       <v-btn
+        class="d-flex"
         href="https://discord.gg/sjtNgQvJAU"
         target="_blank"
         text
@@ -26,6 +28,7 @@
         target="_blank"
         text
         icon
+        class="d-flex"
       >
         <v-icon>mdi-email</v-icon>
       </v-btn>
@@ -38,13 +41,13 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue'
 
 export default Vue.extend({
-  name: "App",
+  name: 'App',
 
   data: () => ({
     //
-  }),
-});
+  })
+})
 </script>
